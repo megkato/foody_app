9521322e10a0e75d5c69758392acd6833edd6d3e
megkato <meg.8.xxx@i.softbank.jp>
2020-08-24 21:47:25 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,31 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+  has_one :sending_destination, dependent: :destroy
+  has_one :credit_card, dependent: :destroy
+  has_many :comments
+  belongs_to :user_type
   
-  validates :name, presence: true, uniqueness: true
+  # belongs_to :item
+
+  validates :nickname,
+      :email,
+      :birthday, presence: true
+  devise    :validatable, password_length: 7..128
+  validates :family_name, presence: true, presence: {message: '姓を入力してください'}
+  validates :first_name, presence: true, presence: {message: '名を入力してください'}
+  validates :family_name_kana, presence: true, presence: {message: '姓カナを入力してください'}
+  validates :first_name_kana, presence: true, presence: {message: '名カナを入力してください'}
+  validates :user_type, presence: true
+  validates :email, uniqueness: true
+  validates :first_name,
+      :family_name,
+      :first_name_kana,
+      :family_name_kana,
+      format: {
+        with: /[^ -~｡-ﾟ]+/,
+        message: '全角で入力してください'
+      }
 
 end
