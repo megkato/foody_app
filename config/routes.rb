a2b1b8b4ef7529c182732ec46bf1517df47908a3
megkato <meg.8.xxx@i.softbank.jp>
2020-08-31 21:42:26 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "sells#index"
   resources :users, only: [:edit, :update]
+  resources :logout, only: [:index]
   resources :sells, only: [:index]
   resources :mypage, only: [:index]
 end
